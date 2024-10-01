c15428e9cfabdbd858a4604704f7dad0a378f35d
anita-ebube <oluebubechukwuanita@gmail.com>
2024-10-01 02:00:30 +0100

    Removed the slides

--- src/components/Services/page.jsx
+++ src/components/Services/page.jsx
@@ -6,11 +6,9 @@ import Sanitation from "../../assets/images/sanitation.jpg";
 export const Services = () => {
     return (
         <div id='services'>
-            <div className='px-4 lg:px-14 max-w-screen-2xl mx-auto my-8'>
+            <div className='px-4 lg:px-14 max-w-screen-2xl mx-auto lg:my-8'>
                 <h4 className='text-4xl text-neutralGrey font-semibold text-center mt-20 mb-10 lg:my-20'>Our Services</h4>
-                {/* <p className='text-neutralGrey text-center mb-20'>GreenFundus offers the following services</p> */}
-
-                {/* First Service */}
+               
                 <div className='md:w-11/12 mx-auto flex flex-col md:flex-row justify-start items-start gap-12'>
                     <div className='order-1 md:order-2 md:w-9/12 mx-auto'>
                         <h2 className='lg:text-4xl text-2xl text-neutralGrey font-semibold mb-8'>Sustainable Agricultural Solutions:</h2>
--- src/pages/Home/EmblaCarousel.jsx
+++ src/pages/Home/EmblaCarousel.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import useEmblaCarousel from 'embla-carousel-react';
-import Home from '../../assets/images/home2.jpeg'
-import Slide2 from "../../assets/images/gallery/slide_3.jpg";
-import Slide3 from "../../assets/images/gallery/slide_4.jpg";
-import Slide from "../../assets/images/home2.jpeg";
+import Slide from "../../assets/images/slide_9.jpg";
 import Autoplay from 'embla-carousel-autoplay';
 import { Navbar } from '../../components/Navbar/page';
 import { Footer } from '../../components/Footer/page';
@@ -15,23 +12,14 @@ export function EmblaCarousel() {
 
   return (
     <div>
-    <Navbar/>
-    <div className="embla" ref={emblaRef}>
-      <div className="embla__container">
-        <div className="embla__slide relative "> 
-          <img src={Home} alt="landing image" className='opacity-1 w-full h-auto' />
-        </div>
-        <div className="embla__slide">
-          <img src={Slide2} alt="landing image" />
-        </div>
-        <div className="embla__slide">
-          <img src={Slide3} alt="landing image" />
-        </div>
+      <Navbar />
+      <div>
+        <img src={Slide} alt="landing image" className='w-full h-auto max-h-screen object-cover' />
       </div>
-    </div>
-    <Mission />
-    <Services />
-    <Footer />
+
+      <Mission />
+      <Services />
+      <Footer />
     </div>
   );
 }
