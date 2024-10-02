687d539cce91af27e0157a61bbf18d07440ca12a
anita-ebube <oluebubechukwuanita@gmail.com>
2024-10-02 10:02:46 +0100

    Added a vercel json file in my public folder

--- src/pages/Home/EmblaCarousel.jsx
+++ src/pages/Home/EmblaCarousel.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import useEmblaCarousel from 'embla-carousel-react';
 import Slide from "../../assets/images/slide_9.jpg";
 import Autoplay from 'embla-carousel-autoplay';
-// import { Mission } from '../../components/Mission/page';
+import { Mission } from '../../components/Mission/page';
 import { Services } from '../../components/Services/page';
 import { Values } from '../../components/Values/page';
 import { Layout } from '../../components/Layout/page';
@@ -35,7 +35,7 @@ export function EmblaCarousel() {
           </button>
         </div>
       </div>
-      {/* <Mission /> */}
+      <Mission />
       <Values />
       <Services />
     </Layout>
